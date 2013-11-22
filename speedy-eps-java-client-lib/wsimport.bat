75b0f615265e8d79b7f8beb207c3e802e95215cd
Mladen Aleksiev <mladen@omg-m.com>
2013-11-22 10:36:12 +0200

    remove wsimport

--- speedy-eps-java-client-lib/wsimport.bat
+++
@@ -1,6 +0,0 @@
-rmdir /S /Q wsimport
-mkdir wsimport
-mkdir wsimport\src
-"%JAVA_HOME%"\bin\wsimport -Xnocompile -s wsimport\src -wsdllocation https://www.speedy.bg/eps/main01.wsdl -keep http://87.120.200.234:8080/eps/mainservice01?wsdl -p com.omgm.speedy.eps.soap.model
-
-REM "%JAVA_HOME%"\bin\wsimport -Xnocompile -s wsimport\src -wsdllocation https://www.speedy.bg/eps/main01.wsdl -keep https://www.speedy.bg/eps/main01.wsdl -p com.omgm.speedy.eps.soap.model
